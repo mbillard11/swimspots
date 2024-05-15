5a60b400873a080917f375817a0d976bcf9133d5
mbillard11 <mbillard11@gmail.com>
2024-05-15 00:21:19 -0400

    Center landing page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="vstack gap-10">
+  <div class="landing">
     <font-awesome-icon icon="fa-solid fa-person-swimming" />
     <h1>Welcome to SwimSpots</h1>
+    Coming soon
   </div>
 </template>
 
 <style scoped>
 .landing {
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
   padding: 10px;
   color: #fff;
+  height: 100vh;
 }
 </style>
